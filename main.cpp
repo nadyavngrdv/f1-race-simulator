c95404d7c67cbc63a9925391ff77c578645c9595
nadya_vino <nadyavngrdv@gmail.com>
2026-08-18 17:19:55 +0300

    Добавлен расчет времени одного круга

--- main.cpp
+++ main.cpp
@@ -2,7 +2,15 @@
 
 int main()
 {
-    std::cout << "Hello, F1 Race Simulator!\n";
+    const double baseLapTime = 90.3; 
+    const double driverModifier = -0.7; 
+    const double carModifier = -1.8;
+    const double penaltyTime = 0.0;
+
+    const double lapTime = 
+    baseLapTime + driverModifier + carModifier + penaltyTime;
+    
+    std::cout << "Lap time: " << lapTime << '\n';
 
     return 0;
 }
